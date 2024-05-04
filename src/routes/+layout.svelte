a381e12967825abfecbac53e772ea6d199100df0
Ummar <ummar.asyraf7@gmail.com>
2024-05-05 00:52:43 +0800

    my wedding card

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { Modal } from "@skeletonlabs/skeleton";
-    import { initializeStores } from '@skeletonlabs/skeleton';
-    import "../app.css";
+  import { Modal } from "@skeletonlabs/skeleton";
+  import { initializeStores } from "@skeletonlabs/skeleton";
+  import "../app.css";
 
-    initializeStores();
+  initializeStores();
 </script>
 
-<Modal/>
+<Modal />
 
 <slot />
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-	throw redirect(307, '/majlis-bertandang-aiman-dan-nuryn');
+	throw redirect(307, '/majlis-pernikahan-ummar-dan-puteri');
 }
